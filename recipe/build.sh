201555f9fbfe912b7c5439c2b640c24c54deea0b
Lynne Jones <ljones.uw@gmail.com>
2023-02-02 12:29:29 -0800

    Add explicit location of f2py

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,8 @@
 echo "gfortran = $(which gfortran)"
 [[ ! -f "$BUILD_PREFIX/bin/gfortran" && ! -z "$GFORTRAN" ]] && ln -s "$GFORTRAN" "$BUILD_PREFIX/bin/gfortran"
 echo "gfortran = $(which gfortran)"
+echo "f2py = $(which f2py)"
 
-./configure gfortran opt --prefix="$PREFIX" --with-pyoorb
+./configure gfortran opt --prefix="$PREFIX" --with-pyoorb --with-f2py=f2py 
 make
 make install
