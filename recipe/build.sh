a07348e8f11ee3f25f8167adc8a8c97da1eea19c
Eli Rykoff <erykoff@stanford.edu>
2023-02-02 21:06:07 -0800

    Try hacking the f2py shebang.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,13 @@ echo "gfortran = $(which gfortran)"
 [[ ! -f "$BUILD_PREFIX/bin/gfortran" && ! -z "$GFORTRAN" ]] && ln -s "$GFORTRAN" "$BUILD_PREFIX/bin/gfortran"
 echo "gfortran = $(which gfortran)"
 
+# If doing cross-compiling, hack the shebang for the f2py executable.
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  shebang="#\!$PREFIX/bin/python"
+  f2py_executable=`which f2py`
+  sed -i "1s%.*%$shebang%" $f2py_executable
+fi
+
 ./configure gfortran opt --prefix="$PREFIX" --with-pyoorb
 make
 make install
