83b5581bb2509b14775d0b0ccbd2dbde9b36f604
Eli Rykoff <erykoff@stanford.edu>
2023-02-04 12:17:05 -0800

    Update shebang hack to point to correct path.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,18 +5,14 @@ echo "gfortran = $(which gfortran)"
 [[ ! -f "$BUILD_PREFIX/bin/gfortran" && ! -z "$GFORTRAN" ]] && ln -s "$GFORTRAN" "$BUILD_PREFIX/bin/gfortran"
 echo "gfortran = $(which gfortran)"
 
-# If doing cross-compiling, hack the shebang for the f2py executable.
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-  echo $PREFIX
-  echo ${PREFIX}
-  echo $BUILD_PREFIX
-  echo ${BUILD_PREFIX}
-  shebang="#\!${BUILD_PREFIX}/bin/python"
-  f2py_executable=`which f2py`
-  echo "Hacking shebang: " $shebang
-  echo "Into: " $f2py_executable
-  sed -i "1s%.*%$shebang%" $f2py_executable
-  cat $f2py_executable
+# If doing cross-compilation, we need to hack the f2py script to point to the right binary.
+# This is a workaround for https://github.com/conda-forge/cross-python-feedstock/issues/64
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]] && [[ `uname -s` == "Darwin" ]]; then
+    shebang="#\!${BUILD_PREFIX}/venv/build/bin/python"
+    f2py_executable=`which f2py`
+    echo "Hacking shebang: " $shebang
+    echo "Into: " $f2py_executable
+    sed -i "1s%.*%$shebang%" $f2py_executable
 fi
 
 ./configure gfortran opt --prefix="$PREFIX" --with-pyoorb
