3567fdcca3961ac552b2897dd42e3a2216228b9e
Eli Rykoff <erykoff@stanford.edu>
2023-02-04 21:44:13 -0800

    Hack the fortran flags.

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,12 +7,19 @@ echo "gfortran = $(which gfortran)"
 
 # If doing cross-compilation, we need to hack the f2py script to point to the right binary.
 # This is a workaround for https://github.com/conda-forge/cross-python-feedstock/issues/64
+# We also hack the FORTRANFLAGS and FFLAGS to remove the arch specification which does
+# not seem to work.
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]] && [[ `uname -s` == "Darwin" ]]; then
     shebang="#\!${BUILD_PREFIX}/venv/build/bin/python"
     f2py_executable=`which f2py`
     echo "Hacking shebang: " $shebang
     echo "Into: " $f2py_executable
     sed -i "1s%.*%$shebang%" $f2py_executable
+
+    tmp=`echo $FFLAGS | sed -e 's/-march[^ ]* //g'`
+    export FFLAGS=$tmp
+    tmp=`echo $FORTRANFLAGS | sed -e 's/-march[^ ]* //g'`
+    export FORTRANFLAGS=$tmp
 fi
 
 ./configure gfortran opt --prefix="$PREFIX" --with-pyoorb
