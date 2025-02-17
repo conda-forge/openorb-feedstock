e56c831fa4bfc40636257814996f0770d65fdc3d
beckermr <becker.mr@gmail.com>
2025-02-17 13:35:37 -0600

    fix: what is this?

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,8 @@
 ./configure gfortran opt --prefix="${PREFIX}" --with-pyoorb \
   --with-f2py=$(which f2py) --with-python="${PYTHON}"
 make || {
+    ls -lah $SRC_DIR/build/_pyoorb_build/bbdir/meson-private/sanitycheckf.exe
+    . $SRC_DIR/build/_pyoorb_build/bbdir/meson-private/sanitycheckf.exe
     cat $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt
     exit 1
 }
