8e0eefb95252f1633c27e517e2512b38b93aa150
beckermr <becker.mr@gmail.com>
2025-02-17 13:27:20 -0600

    debug: what is here

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,8 @@
 
 ./configure gfortran opt --prefix="${PREFIX}" --with-pyoorb \
   --with-f2py=$(which f2py) --with-python="${PYTHON}"
-make
+make || {
+    cat $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt
+    exit 1
+}
 make install
