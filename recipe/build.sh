a52db6bc0ce551e4d1f0af8c516bfb43037de036
beckermr <becker.mr@gmail.com>
2025-02-17 17:29:16 -0600

    fix: get meson to build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,9 @@
 
 set -ex
 
+export FFLAGS="${FFLAGS} -I../../../build -L../../../lib"
+
 ./configure gfortran opt --prefix="${PREFIX}" --with-pyoorb \
   --with-f2py=$(which f2py) --with-python="${PYTHON}"
-make || {
-    otool -L $SRC_DIR/main/oorb.tmp
-    ls -lah $SRC_DIR/build/_pyoorb_build/bbdir/meson-private/sanitycheckf.exe
-    . $SRC_DIR/build/_pyoorb_build/bbdir/meson-private/sanitycheckf.exe
-    cat $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt
-    exit 1
-}
+make -j${CPU_COUNT}
 make install
