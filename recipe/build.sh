1cd8d610fb9c8bb0b91a94fe8c094830aebbf313
beckermr <becker.mr@gmail.com>
2025-02-17 23:14:08 -0600

    fix: see what is in the logs

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,5 +6,8 @@ export FFLAGS="${FFLAGS} -I../../../build -L../../../lib"
 
 ./configure gfortran opt --prefix="${PREFIX}" --with-pyoorb \
   --with-f2py=$(which f2py) --with-python="${PYTHON}"
-make -j${CPU_COUNT}
+make -j${CPU_COUNT} || {
+    cat $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt
+    exit 1
+}
 make install
