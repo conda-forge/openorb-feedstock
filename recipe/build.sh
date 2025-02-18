6d2793ad8bbd81b2b450f233ab37c2f0bbe0f709
beckermr <becker.mr@gmail.com>
2025-02-17 23:45:20 -0600

    fix: try this adaptive setting

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,20 @@
 
 set -ex
 
-export FFLAGS="${FFLAGS} -I../../../build -L../../../lib"
+# meson is used on 3.12 and is one more directory down than where
+# distutils was
+if [[ "$(python -V)" == "*3.12.*" ]]; then
+  # CONDA_LIBORB is hacked in via a patch
+  export CONDA_LIBORB="../../lib/liboorb.a"
+  export FFLAGS="${FFLAGS} -I../../../build -L../../../lib"
+else
+  export CONDA_LIBORB="../lib/liboorb.a"
+fi
 
 ./configure gfortran opt --prefix="${PREFIX}" --with-pyoorb \
   --with-f2py=$(which f2py) --with-python="${PYTHON}"
 make -j${CPU_COUNT} || {
-    cat $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt
-    exit 1
+  cat $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt
+  exit 1
 }
 make install
