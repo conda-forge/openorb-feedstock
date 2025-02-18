1dd0c821f45b23fca300d582ec0344a58a6997b6
beckermr <becker.mr@gmail.com>
2025-02-18 11:40:05 -0600

    fix: no parallel jobs

--- recipe/build.sh
+++ recipe/build.sh
@@ -35,7 +35,7 @@ fi
 
 ./configure gfortran opt --prefix="${PREFIX}" --with-pyoorb \
   --with-f2py=$(which f2py) --with-python="${PYTHON}"
-make -j${CPU_COUNT} || {
+make || {
   cat $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt
   exit 1
 }
