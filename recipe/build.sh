4c6436b5345bac712a327367d2e6dfc78989a056
beckermr <becker.mr@gmail.com>
2025-02-18 01:48:20 -0600

    fix: set adaptiely

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,19 @@
 
 set -ex
 
+if [[ "$(basename ${CONFIG_FILE})" == *_python3.12* ]]; then
+  export CONDA_LIBORB="pyoorb.pyf ../main/io.f90 ../python/*.f90 ../classes/*.f90 ../modules/*.f90"
+else
+  export CONDA_LIBORB="-m pyoorb pyoorb.o pyoorb.pyf ../lib/liboorb.a"
+fi
+
 ./configure gfortran opt --prefix="${PREFIX}" --with-pyoorb \
   --with-f2py=$(which f2py) --with-python="${PYTHON}"
-make -j${CPU_COUNT}
+make -j${CPU_COUNT} || {
+  cat $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt
+  exit 1
+}
+
+cat ${SRC_DIR}/build/_pyoorb_build/meson.build
+
 make install
