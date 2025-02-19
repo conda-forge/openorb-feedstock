81ce53e5234b1d06b69483cffce58dd4c138a08d
beckermr <becker.mr@gmail.com>
2025-02-18 21:51:11 -0600

    fix: properly inject cross file

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,24 +2,33 @@
 
 set -ex
 
-build_numpy=$(${BUILD_PREFIX}/bin/python -c 'import site; print(site.getsitepackages()[0])')/numpy
-host_numpy=$(${PREFIX}/bin/python -c 'import site; print(site.getsitepackages()[0])')/numpy
+echo "BUILD_PREFIX: ${BUILD_PREFIX//p/P}"
+echo "PREFIX: ${PREFIX//p/P}"
+echo "CONDA_PREFIX: ${CONDA_PREFIX//p/P}"
 
-if [ -d "${build_numpy}" ]; then
-  if [[ "${build_numpy}" != "${host_numpy}" ]]; then
-    pushd "${build_numpy}/f2py/_backends"
-    patch -b -p1 _meson.py < ${RECIPE_DIR}/f2py_meson.patch
-    rm -rf __pycache__
+build_numpy=${BUILD_PREFIX}/lib/python$(${BUILD_PREFIX}/bin/python -c "import sys; print('.'.join(sys.version.split('.')[0:2]))")/site-packages/numpy
+if [[ "${build_platform}" != "${target_platform}" ]]; then
+  build_venv_numpy=${BUILD_PREFIX}/venv/lib/python$(${BUILD_PREFIX}/venv/bin/build-python -c "import sys; print('.'.join(sys.version.split('.')[0:2]))")/site-packages/numpy
+else
+  build_venv_numpy=""
+fi
+host_numpy=${PREFIX}/lib/python$(${PREFIX}/bin/python -c "import sys; print('.'.join(sys.version.split('.')[0:2]))")/site-packages/numpy
+
+echo "build_numpy: ${build_numpy}"
+echo "build_venv_numpy: ${build_venv_numpy}"
+echo "host_numpy: ${host_numpy}"
+
+for npdir in ${build_numpy} ${build_venv_numpy} ${host_numpy}; do
+  if [ -d "${npdir}/f2py/_backends" ]; then
+    pushd "${npdir}/f2py/_backends"
+    if [ ! -f _meson.py.orig ]; then
+      echo "patched numpy at ${npdir}"
+      patch -b -p1 _meson.py < ${RECIPE_DIR}/f2py_meson.patch
+      rm -rf __pycache__
+    fi
     popd
   fi
-fi
-
-if [ -d "${host_numpy}" ]; then
-  pushd "${host_numpy}/f2py/_backends"
-  patch -b -p1 _meson.py < ${RECIPE_DIR}/f2py_meson.patch
-  rm -rf __pycache__
-  popd
-fi
+done
 
 if [[ "${CONFIG}" == *_python3.12* ]]; then
   export MESON_ARGS=${MESON_ARGS//--prefix=${PREFIX}/""}
@@ -28,7 +37,7 @@ if [[ "${CONFIG}" == *_python3.12* ]]; then
     echo "meson cross file contents:"
     cat ${F2PY_MESON_CROSS_FILE}
   fi
-  export CONDA_LIBORB="pyoorb.pyf ../main/io.f90 ../python/*.f90 ../classes/*.f90 ../modules/*.f90 ${MESON_ARGS}"
+  export CONDA_LIBORB="pyoorb.pyf ../main/io.f90 ../python/*.f90 ../classes/*.f90 ../modules/*.f90"
   export FFLAGS="${FFLAGS} -I${SRC_DIR}/build"
   export CFLAGS="${CFLAGS} -I${SRC_DIR}/build"
 else
@@ -43,24 +52,14 @@ make || {
 }
 make install
 
-if [ -d "${build_numpy}" ]; then
-  if [[ "${build_numpy}" != "${host_numpy}" ]]; then
-    pushd "${build_numpy}/f2py/_backends"
-    mv _meson.py.orig _meson.py
-    rm -rf __pycache__
+for npdir in ${build_numpy} ${build_venv_numpy} ${host_numpy}; do
+  if [ -d "${npdir}/f2py/_backends" ]; then
+    pushd "${npdir}/f2py/_backends"
+    if [ -f _meson.py.orig ]; then
+      echo "restored numpy at ${npdir}"
+      mv _meson.py.orig _meson.py
+      rm -rf __pycache__
+    fi
     popd
   fi
-fi
-
-if [ -d "${host_numpy}" ]; then
-  pushd "${host_numpy}/f2py/_backends"
-  mv _meson.py.orig _meson.py
-  rm -rf __pycache__
-  popd
-fi
-
-if [ -d "${BUILD_PREFIX}/venv" ]; then
-  ls -lah ${BUILD_PREFIX}/venv/*
-else
-  echo "no venv found!"
-fi
+done
