ae1946ff0fdbeffc84cdadeb05fe4dd601dca473
beckermr <becker.mr@gmail.com>
2025-02-19 07:11:31 -0600

    fix: a little clean up to make patches more robust and comments

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# these bits of bash patch f2py in numpy to use the meson --cross-file
+# when cross-compiling with meson on python 3.12 or higher
+# see https://github.com/numpy/numpy/issues/28352
 echo "BUILD_PREFIX: ${BUILD_PREFIX//p/P}"
 echo "PREFIX: ${PREFIX//p/P}"
 echo "CONDA_PREFIX: ${CONDA_PREFIX//p/P}"
@@ -30,14 +33,20 @@ for npdir in ${build_numpy} ${build_venv_numpy} ${host_numpy}; do
   fi
 done
 
+# on python 3.12 and higher, we have to use meson to build
 if [[ "${CONFIG}" == *_python3.12* ]]; then
-  export MESON_ARGS=${MESON_ARGS//--prefix=${PREFIX}/""}
+  # here we setup the cross-compiled builds with meson if needed
   if [[ "${build_platform}" != "${target_platform}" ]]; then
     export F2PY_MESON_CROSS_FILE=${BUILD_PREFIX}/meson_cross_file.txt
     echo "meson cross file contents:"
     cat ${F2PY_MESON_CROSS_FILE}
   fi
-  export CONDA_LIBORB="pyoorb.pyf ../main/io.f90 ../python/*.f90 ../classes/*.f90 ../modules/*.f90"
+  # meson does not appear to like having .o or .a files directly passed to f2py
+  # so we pass all of the f90 sources instead
+  # we use single quotes to prevent the shell from expanding LIBSOURCES
+  # until the build is running
+  export CONDA_LIBORB='pyoorb.pyf ../python/*.f90 ${LIBSOURCES}'
+  # these includes are needed to get the mod files
   export FFLAGS="${FFLAGS} -I${SRC_DIR}/build"
   export CFLAGS="${CFLAGS} -I${SRC_DIR}/build"
 else
@@ -47,11 +56,15 @@ fi
 ./configure gfortran opt --prefix="${PREFIX}" --with-pyoorb \
   --with-f2py=$(which f2py) --with-python="${PYTHON}"
 make || {
-  cat $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt
+  if [ -f $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt ]; then
+    echo "meson logs:"
+    cat $SRC_DIR/build/_pyoorb_build/bbdir/meson-logs/meson-log.txt
+  fi
   exit 1
 }
 make install
 
+# now we undo the f2py patch
 for npdir in ${build_numpy} ${build_venv_numpy} ${host_numpy}; do
   if [ -d "${npdir}/f2py/_backends" ]; then
     pushd "${npdir}/f2py/_backends"
